80d150e1f219944c24bea30cf8d365ef6d7f3581
changeAJay <ajajay008@gmail.com>
2022-03-29 21:43:34 +0530

    deleted unnecessary code

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,11 @@
+import requests
+import data_manager
+import flight_search
+
+s_data_endpoint = "https://api.sheety.co/308a6d2782f06fc8638c04aff1163efc/flightDeals/prices"
+
+
 class FlightData:
     #This class is responsible for structuring the flight data.
     pass
+
