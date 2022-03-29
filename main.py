b2fa1f61f4fcee1be25b009b390ddf75c4cf0bc7
changeAJay <ajajay008@gmail.com>
2022-03-29 21:40:44 +0530

    added iatacode to the data in sheet

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #This file will need to use the DataManager,FlightSearch, FlightData,
 #NotificationManager classes to achieve the program requirements.
+from pprint import pprint
 
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 data = DataManager()
-sheet_data = data.response
-print(sheet_data)
 
-flight_search = FlightSearch()
-flight_search_data = flight_search.search_response
+sheet = data.define_data()
+# print(sheet)
 
-print(flight_search_data.json())
+
+if sheet[0]["iataCode"] == "":
+    flight_search = FlightSearch()
+    for row in sheet:
+        row["iataCode"] = flight_search.get_code(row["city"])
+    data.get_data = sheet
+    data.update_data()
+
+
