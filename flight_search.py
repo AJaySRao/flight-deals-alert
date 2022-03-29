636dc4ac3f44dc2e925db3210775f0bad42367b2
changeAJay <ajajay008@gmail.com>
2022-03-29 09:38:45 +0530

    IATA code extraction with city names using search API

--- flight_search.py
+++ flight_search.py
@@ -2,11 +2,7 @@ import requests
 import os
 
 flight_endpoint = "https://tequila-api.kiwi.com/locations/query"
-details = {
-    "term": "USA",
-    "location_types": "airport",
-    "locale": "en-US"
-    }
+
 header = {
     "apikey": "iWqm-26D0V_tjhwhaMp9bl512ZV8JdX3"
 }
@@ -14,7 +10,11 @@ header = {
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    def __init__(self):
-
-        self.search_response = requests.get(url=flight_endpoint, params=details, headers=header)
+    def __init__(self, city_name):
+        self.city_name = city_name
+        self.details = {
+            "term": self.city_name,
+            "locale": "en-US"
+        }
+        self.search_response = requests.get(url=flight_endpoint, params=self.details, headers=header)
 
