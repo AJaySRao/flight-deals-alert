f7075d00fc9ee6c3f67dc392e92349adeb22d559
changeAJay <ajajay008@gmail.com>
2022-03-30 08:00:01 +0530

    Gets fare details

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #This file will need to use the DataManager,FlightSearch, FlightData,
 #NotificationManager classes to achieve the program requirements.
-from pprint import pprint
+import datetime
 
 from data_manager import DataManager
 from flight_search import FlightSearch
@@ -11,12 +11,22 @@ data = DataManager()
 sheet = data.define_data()
 # print(sheet)
 
-
+# Filling blank spaces with IATA codes in sheets
 if sheet[0]["iataCode"] == "":
     flight_search = FlightSearch()
     for row in sheet:
         row["iataCode"] = flight_search.get_code(row["city"])
     data.get_data = sheet
     data.update_data()
+
+# Fare
+flight_data = FlightData()
+for code in sheet:
+    fare_details = flight_data.get_flight_details(city_code=code['iataCode'])
+    print(fare_details)
+
+
+
+
 
 
