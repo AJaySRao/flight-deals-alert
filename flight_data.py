35eea1228c82e3ec0294e418720bc49b53811e55
changeAJay <ajajay008@gmail.com>
2022-03-30 08:02:26 +0530

    searches for the cheapest flight for the required route

--- flight_data.py
+++ flight_data.py
@@ -1,11 +1,33 @@
 import requests
-import data_manager
-import flight_search
+import datetime
 
-s_data_endpoint = "https://api.sheety.co/308a6d2782f06fc8638c04aff1163efc/flightDeals/prices"
+flight_endpoint = "https://tequila-api.kiwi.com/v2/search"
 
+header = {
+    "apikey": "iWqm-26D0V_tjhwhaMp9bl512ZV8JdX3"
+}
+TODAY = datetime.datetime.now()
+SIX_MONTHS = TODAY+datetime.timedelta(days=180)
 
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+    def get_flight_details(self, city_code):
+        details = {
+            "max_stopovers": 0,
+            "fly_from": "LON",
+            "fly_to": city_code,
+            "one_for_city": 1,
+            "date_from": TODAY.strftime("%d/%m/%Y"),
+            "date_to": SIX_MONTHS.strftime("%d/%m/%Y"),
+            "curr": "GBP",
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+        }
+        search_response = requests.get(url=flight_endpoint, params=details, headers=header)
+        flights = search_response.json()
+        flights_slice = flights["data"][:1]
+        city = flights_slice[0]["cityTo"]
+        fare = flights_slice[0]["price"]
 
+        return f"{city}: £{fare}"
