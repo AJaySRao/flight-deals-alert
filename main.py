36ffb97b417ade8846485b1f1bb9d76b71f5b9d2
changeAJay <ajajay008@gmail.com>
2022-03-30 10:08:28 +0530

    Triggers notification alerts

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import FlightData
+from notification_manager import NotificationManager
 
 data = DataManager()
 
@@ -12,21 +13,24 @@ sheet = data.define_data()
 # print(sheet)
 
 # Filling blank spaces with IATA codes in sheets
-if sheet[0]["iataCode"] == "":
-    flight_search = FlightSearch()
-    for row in sheet:
-        row["iataCode"] = flight_search.get_code(row["city"])
-    data.get_data = sheet
-    data.update_data()
+# if sheet[0]["iataCode"] == "":
+#     flight_search = FlightSearch()
+#     for row in sheet:
+#         row["iataCode"] = flight_search.get_code(row["city"])
+#     data.get_data = sheet
+#     data.update_data()
 
 # Fare
 flight_data = FlightData()
-for code in sheet:
-    fare_details = flight_data.get_flight_details(city_code=code['iataCode'])
-    print(fare_details)
+notification = NotificationManager()
+for row in sheet:
+    fare_details = flight_data.get_flight_details(city_code=row['iataCode'])
+    check = fare_details
+    sheet_details = (row['city'], row['lowestPrice'])
+    if check[1] < sheet_details[1]:
+        notification.send_msg(
+            body=f"Lowest Price Alert! "
+                 f"only ${check[1]} to fly from London to {check[0]}, from {check[2]} to {check[3]}")
 
 
-
-
-
-
+# fare_details = flight_data.get_flight_details(city_code="CPT")
