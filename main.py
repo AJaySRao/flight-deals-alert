e1aa62b9637785f7a589353f039f896a6c8813ae
changeAJay <ajajay008@gmail.com>
2022-03-31 09:34:34 +0530

    Added from location and updated msg body

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 #This file will need to use the DataManager,FlightSearch, FlightData,
 #NotificationManager classes to achieve the program requirements.
 import datetime
-
 from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import FlightData
 from notification_manager import NotificationManager
 
+
+FROM = "LON"
+
 data = DataManager()
+flight_search = FlightSearch()
 
 sheet = data.define_data()
 # print(sheet)
 
 # Filling blank spaces with IATA codes in sheets
 if sheet[0]["iataCode"] == "":
-    flight_search = FlightSearch()
-    for row in sheet:
-        row["iataCode"] = flight_search.get_code(row["city"])
+    for city in sheet:
+        city["iataCode"] = flight_search.get_code(city["city"])
     data.get_data = sheet
     data.update_data()
 
+
 # Fare
-flight_data = FlightData()
 notification = NotificationManager()
-for row in sheet:
-    fare_details = flight_data.get_flight_details(city_code=row['iataCode'])
-    check = fare_details
-    sheet_details = (row['city'], row['lowestPrice'])
-    if check[1] < sheet_details[1]:
+for destination in sheet:
+    flight = flight_search.get_flight_details(origin_city=FROM, city_code=destination['iataCode'])
+
+    if flight.price < destination['lowestPrice']:
         notification.send_msg(
-            body=f"Lowest Price Alert! "
-                 f"only ${check[1]} to fly from London to {check[0]}, from {check[2]} to {check[3]}")
+            body=f"Low price alert! "
+                 f"Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "
+                 f"{flight.destination_city}-{flight.destination_airport}, "
+                 f"from {flight.out_date} to {flight.return_date}."
+        )
 
 
 # fare_details = flight_data.get_flight_details(city_code="CPT")
