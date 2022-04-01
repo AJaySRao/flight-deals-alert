21deebd0cefb89b05667f52e3c27725c73f980a5
changeAJay <ajajay008@gmail.com>
2022-04-01 09:39:11 +0530

    Updated Registration process

--- main.py
+++ main.py
@@ -4,36 +4,41 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import FlightData
 from notification_manager import NotificationManager
-
-
-FROM = "LON"
-
+#
+#
+# FROM = "LON"
+#
 data = DataManager()
-flight_search = FlightSearch()
+# flight_search = FlightSearch()
+#
+# sheet = data.define_data()
+# # print(sheet)
+#
+# # Filling blank spaces with IATA codes in sheets
+# if sheet[0]["iataCode"] == "":
+#     for city in sheet:
+#         city["iataCode"] = flight_search.get_code(city["city"])
+#     data.get_data = sheet
+#     data.update_data()
+#
+#
+# # Fare
+# notification = NotificationManager()
+# for destination in sheet:
+#     flight = flight_search.get_flight_details(
+#         origin_city=FROM,
+#         city_code=destination['iataCode']
+#     )
+#
+#     if flight.price < destination['lowestPrice']:
+#         notification.send_msg(
+#             body=f"Low price alert! "
+#                  f"Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "
+#                  f"{flight.destination_city}-{flight.destination_airport}, "
+#                  f"from {flight.out_date} to {flight.return_date}."
+#         )
+from flight_club import FlightClub
 
-sheet = data.define_data()
-# print(sheet)
-
-# Filling blank spaces with IATA codes in sheets
-if sheet[0]["iataCode"] == "":
-    for city in sheet:
-        city["iataCode"] = flight_search.get_code(city["city"])
-    data.get_data = sheet
-    data.update_data()
-
-
-# Fare
-notification = NotificationManager()
-for destination in sheet:
-    flight = flight_search.get_flight_details(
-        origin_city=FROM,
-        city_code=destination['iataCode']
-    )
-
-    if flight.price < destination['lowestPrice']:
-        notification.send_msg(
-            body=f"Low price alert! "
-                 f"Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "
-                 f"{flight.destination_city}-{flight.destination_airport}, "
-                 f"from {flight.out_date} to {flight.return_date}."
-        )
+flight_club = FlightClub()
+flight_club.person_details()
+data.add_data(flight_club.users)
