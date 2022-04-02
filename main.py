52ac251210153807bb3729263844734e8109e7d0
changeAJay <ajajay008@gmail.com>
2022-04-02 16:43:28 +0530

    added message

--- main.py
+++ main.py
@@ -4,41 +4,60 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import FlightData
 from notification_manager import NotificationManager
-#
-#
-# FROM = "LON"
-#
-data = DataManager()
-# flight_search = FlightSearch()
-#
-# sheet = data.define_data()
-# # print(sheet)
-#
-# # Filling blank spaces with IATA codes in sheets
-# if sheet[0]["iataCode"] == "":
-#     for city in sheet:
-#         city["iataCode"] = flight_search.get_code(city["city"])
-#     data.get_data = sheet
-#     data.update_data()
-#
-#
-# # Fare
-# notification = NotificationManager()
-# for destination in sheet:
-#     flight = flight_search.get_flight_details(
-#         origin_city=FROM,
-#         city_code=destination['iataCode']
-#     )
-#
-#     if flight.price < destination['lowestPrice']:
-#         notification.send_msg(
-#             body=f"Low price alert! "
-#                  f"Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "
-#                  f"{flight.destination_city}-{flight.destination_airport}, "
-#                  f"from {flight.out_date} to {flight.return_date}."
-#         )
-from flight_club import FlightClub
 
-flight_club = FlightClub()
-flight_club.person_details()
-data.add_data(flight_club.users)
+
+FROM = "LON"
+
+data = DataManager()
+flight_search = FlightSearch()
+
+sheet = data.define_data()
+# print(sheet)
+
+# Filling blank spaces with IATA codes in sheets
+if sheet[0]["iataCode"] == "":
+    for city in sheet:
+        city["iataCode"] = flight_search.get_code(city["city"])
+    data.get_data = sheet
+    data.update_data()
+
+
+# Fare
+notification = NotificationManager()
+for destination in sheet:
+    flight = flight_search.get_flight_details(
+        origin_city=FROM,
+        city_code=destination['iataCode']
+    )
+    if flight is None:
+        continue
+
+    if flight.price < destination['lowestPrice']:
+        message = f"Low price alert! "\
+                f"Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "\
+                f"{flight.destination_city}-{flight.destination_airport}, "\
+                f"from {flight.out_date} to {flight.return_date}."
+        if flight.stop_over == 1:
+            message += f"\nFlight has {flight.stop_over} stop over, via {flight.via_city}."
+        if flight.stop_over == 2:
+            message += f"\nFlight has {flight.stop_over} stop over, via {flight.via_city} & {flight.via_city2}."
+
+        print(message)
+
+
+# flight = flight_search.get_flight_details(
+#     origin_city=FROM,
+#     city_code=sheet[-1]['iataCode']
+# )
+# if flight is None:
+#     continue
+#
+# if flight.price < sheet[-1]['lowestPrice']:
+#     message = f"Low price alert! "\
+#             f"Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "\
+#             f"{flight.destination_city}-{flight.destination_airport}, "\
+#             f"from {flight.out_date} to {flight.return_date}."
+#     if flight.stop_over > 0:
+#         message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+#
+#     print(message)
