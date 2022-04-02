de911230c2793cf0d159216bfb70be386026cdd3
changeAJay <ajajay008@gmail.com>
2022-04-02 21:07:58 +0530

    Get customer details

--- data_manager.py
+++ data_manager.py
@@ -7,6 +7,7 @@ s_endpoint = "https://api.sheety.co/3f956a9a288358e3b83a7ce2fa7c271f/flightDeals
 class DataManager:
     def __init__(self):
         self.get_data = {}
+        self.customer_data = []
 
     def define_data(self):
         read_response = requests.get(url=s_data_endpoint)
@@ -30,4 +31,11 @@ class DataManager:
     def add_data(self, user_details):
         read_response = requests.post(url=s_endpoint, json=user_details)
         sheet_data = read_response.json()
+
+    def get_customer_emails(self):
+        response = requests.get(url=s_endpoint)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
+
 
